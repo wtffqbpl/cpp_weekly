47c4f30e48effc7813ee1a4d6f9cd3ef8b70353b
Yuanjun Ren <renyj@buaa.edu.cn>
2022-07-13 23:19:18 +0800

    Add pointer-like iterator class

---
+++ pointer_like_classes/iterator_case.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+
+template <typename T>
+struct __my_list_node {
+  void *prev;
+  void *next;
+  T data;
+};
+
+template <typename T, typename Ref, typename Ptr>
+struct __my_list_iterator {
+  typedef __my_list_iterator<T, Ref, Ptr> self;
+  typedef Ptr pointer;
+  typedef Ref reference;
+  typedef __my_list_node<T> *link_type;
+  link_type node;
+  bool operator==(const self &x) const { return node == x; }
+  bool operator!=(const self &x) const { return node != x; }
+  reference operator*() const { return (*node).data; }
+  pointer operator->() const { return &(operator*()); }
+  // &(operator*()) --- 相当于 （&(*iter))
+  self &operator++() { node = (link_type)((*node).next); }
+  self operator++(int) {
+    self tmp = *this;
+    ++*this;
+    return tmp;
+  }
+  self &operator--() {
+    node = (link_type)((*node).prev);
+    return *this;
+  }
+  self operator--(int) {
+    self tmp = *this;
+    --*this;
+    return *this;
+  }
+};
+
+int main() { return 0; }
