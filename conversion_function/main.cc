b1d54b501e566e20680d96a652c7a653e5302d9a
Yuanjun Ren <renyj@buaa.edu.cn>
2022-07-13 22:32:49 +0800

    Add explicit ctor case

--- conversion_function/main.cc
+++ conversion_function/main.cc
@@ -2,14 +2,27 @@
 
 class Fraction {
 public:
-  Fraction() = delete;
-  Fraction operator&=(const Fraction &) = delete;
+  // This is non-explicit-one-argument ctor.
+  //  意思是，只要一个实参即可，两个也行.
+  // explicit Fraction(int num, int den = 1) : numerator_(num),
+  // denominator_(den) {}
 
-  Fraction(int num, int den = 1) : numerator_(num), denominator_(den) {}
+  // explicit-one-agument ctor.
+  // 声明不能隐式地将其他类型转换到该类型.
+  explicit Fraction(int num, int den = 1)
+      : numerator_(num), denominator_(den) {}
 
   operator double() const {
     return static_cast<double>(numerator_) / denominator_;
   }
+
+  Fraction operator+(const Fraction &f) {
+    return Fraction(this->num() + f.num(), this->den() + f.den());
+  }
+
+  int num() const { return numerator_; }
+
+  int den() const { return denominator_; }
 
 private:
   int numerator_;   // fraction
