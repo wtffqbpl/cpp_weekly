04d71f009f0d85a39e323836b91025399dac9e88
Yuanjun Ren <renyj@buaa.edu.cn>
2022-07-13 22:45:51 +0800

    Add pointer-like classes

---
+++ pointer_like_classes/main.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+// 通过这种方式来代理原来的指针。
+template <typename T> class my_shared_ptr {
+public:
+  T &operator*() const { return *px; }
+  // 指针符号-> 可以一直传递下去, 这样就可以理解 sp-> 这个符号指向到Foo
+  // 对象的函数
+  T *operator->() const { return px; }
+
+  my_shared_ptr(T *_p) : px(_p) {}
+
+private:
+  T *px;
+  long *pn;
+};
+
+struct Foo {
+  void method(void) { std::cout << "Foo method." << std::endl; }
+};
+
+int main() {
+  my_shared_ptr<Foo> sp(new Foo);
+
+  sp->method();
+
+  return 0;
+}
