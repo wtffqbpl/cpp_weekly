4d365546f9c1b61fe9a9e17ee00922de37035fde
Yuanjun Ren <renyj@buaa.edu.cn>
2022-08-26 21:49:51 +0800

    add range container

--- stl/vector_test.cc
+++ stl/vector_test.cc
@@ -1,5 +1,6 @@
 #include <deque>
 #include <gtest/gtest.h>
+#include <list>
 
 TEST(vector, test1) {
   std::stringstream oss;
@@ -81,6 +82,53 @@ TEST(vector, emplace_test) {
 
 #ifndef NDEBUG
   std::cout << "expected output:\n"
+            << oss.str() << "actual output:\n"
+            << act_output << std::endl;
+#endif
+
+  EXPECT_TRUE(oss.str() == act_output);
+}
+
+struct value_not_found {};
+struct value_not_found_twice {};
+
+template <typename Range, typename Value>
+void print_interval(const Range &r, const Value &v,
+                    std::ostream &os = std::cout) {
+  auto it = std::find(std::begin(r), std::end(r), v), it2 = it;
+
+  if (it == std::end(r))
+    throw value_not_found{};
+  ++it2;
+
+  auto past = std::find(it2, std::end(r), v);
+  if (past == std::end(r))
+    throw value_not_found_twice{};
+  ++past;
+
+  for (; it != past; ++it)
+    os << *it << ' ';
+  os << '\n';
+}
+
+TEST(stl_range, range_test) {
+  std::stringstream oss;
+  testing::internal::CaptureStdout();
+
+  std::list<int> seq = {3, 4, 7, 9, 2, 5, 7, 8};
+  print_interval(seq, 7);
+  oss << "7 9 2 5 7 \n";
+
+  int array[] = {3, 4, 7, 9, 2, 5, 7, 8};
+  std::stringstream ss;
+  print_interval(array, 7, ss);
+  std::cout << ss.str();
+  oss << ss.str();
+
+  std::string act_output = testing::internal::GetCapturedStdout();
+
+#ifndef NDEBUG
+  std::cout << "Expected output:\n"
             << oss.str() << "actual output:\n"
             << act_output << std::endl;
 #endif
