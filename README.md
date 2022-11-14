cd90d3f04f25bbb3c6bdbab4fa54fb5e4f8b8702
Yuanjun Ren <renyj@buaa.edu.cn>
2022-11-14 23:52:19 +0800

    Add compile-time for-loop testcases

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # cpp_weekly
 
+# Learning Materials
+- [Effective Modern C++, by Scott Meyers](https://www.bilibili.com/video/BV1H7411n7FJ?p=1&vd_source=e4e0ed3c5275b0c54cf800155c1fac0e)
+
 ![workflow](https://github.com/wtffqbpl/cpp_weekly/actions/workflows/cmake.yml/badge.svg)
 [![FOSSA Status](https://app.fossa.com/api/projects/git%2Bgithub.com%2Fwtffqbpl%2Fcpp_weekly.svg?type=shield)](https://app.fossa.com/projects/git%2Bgithub.com%2Fwtffqbpl%2Fcpp_weekly?ref=badge_shield)
 
---
+++ template/for_loop_in_compile_time.cc
@@ -0,0 +1,73 @@
+#include "internal_check_conds.h"
+#include <gtest/gtest.h>
+
+// The basic recursion version.
+namespace {
+
+template <typename... Ts>
+void print(Ts... args) { (std::cout << ... << args) << std::endl; }
+
+template <size_t N, size_t I>
+struct ForEach {
+
+  static void item() {
+    std::cout << I << std::endl;
+
+    // recursive upwards
+    if constexpr (I + 1 < N)
+      ForEach<N, I+1>::item();
+  }
+};
+
+#if 0
+template <size_t N, size_t I = 0, typename... Ts>
+void for_loop_compile_time(Ts... args) {
+  print(args...);
+
+  if constexpr (I + 1 < N)
+    for_loop_compile_time<N, I+1, Ts...>(args...);
+}
+#endif
+
+} // namespace
+
+TEST(for_loop_in_compile_time, basic_test) {
+  std::stringstream oss;
+  testing::internal::CaptureStdout();
+
+  ForEach<10, 0>::item();
+  oss << "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n";
+
+  auto act_output = testing::internal::GetCapturedStdout();
+  debug_msg(oss, act_output);
+  EXPECT_TRUE(oss.str() == act_output);
+}
+
+template <size_t N, size_t I = 0, typename... Ts>
+void for_loop_compile_time(Ts... args) {
+  print(args...);
+
+  if constexpr (I + 1 < N)
+    for_loop_compile_time<N, I+1, Ts...>(args...);
+}
+
+TEST(for_loop_in_compile_time, function_loop_test) {
+  std::stringstream oss;
+  testing::internal::CaptureStdout();
+
+  for_loop_compile_time<10>(1, 2, 3, 4);
+  oss << "1234\n"
+         "1234\n"
+         "1234\n"
+         "1234\n"
+         "1234\n"
+         "1234\n"
+         "1234\n"
+         "1234\n"
+         "1234\n"
+         "1234\n";
+
+  auto act_output = testing::internal::GetCapturedStdout();
+  debug_msg(oss, act_output);
+  EXPECT_TRUE(oss.str() == act_output);
+}
