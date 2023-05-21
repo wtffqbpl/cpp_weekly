a57e463f41a1863ad7603a1d290e6f2552aa937d
Yuanjun Ren <renyj@buaa.edu.cn>
2023-05-21 23:33:21 +0800

    add std::common_type examples

--- template/template_design_examples.cc
+++ template/template_design_examples.cc
@@ -129,3 +129,74 @@ private:
 };
 
 } // namespace mtl
+
+namespace variadic_template_type_deduction {
+// If you want to deduce the variadic template type, the variadic template
+// struct can be used for type deduction. note: variadic template function
+// cannot use variadic template parameters type deduction.
+
+// 递归函数和递归类的编译行为是不同的，后者可递归实例化，而前者不行。这就是为什么可以在
+// 可变参数类中递归使用 decltype, 但不能在可变参数函数中使用decltype.
+
+template <typename T> inline T sum(T t) { return t; }
+
+/// \code
+/// template <typename T, typename... P>
+/// auto sum(T t, P... p) -> decltype(t + sum(p...)) // This variadic type
+/// deduction
+///                                                  // would be failed.
+/// {
+///   return t + sum(p...);
+/// }
+/// \endcode
+
+template <typename... P> struct sum_type;
+
+template <typename T> struct sum_type<T> { using type = T; };
+
+template <typename T, typename... P> struct sum_type<T, P...> {
+  using type = decltype(T{} + typename sum_type<P...>::type{});
+};
+
+template <typename... P> using sum_type_t = typename sum_type<P...>::type;
+
+template <typename T, typename... P>
+inline sum_type_t<T, P...> sum_final(T t, P... p) {
+  return t + sum(p...);
+}
+
+// std::common_type
+// https://en.cppreference.com/w/cpp/types/common_type
+// Determines the common type among all types T..., that is the type all T...
+// can be implicitly converted to. If such a type exists (as determined
+// according to the rules below), the member type names that type. Otherwise,
+// there is no member type.
+//  * if `sizeof...(T)` is zero, there is no member type.
+//  * if `sizeof...(T)` is one (i.e., T... contains only one type T0), the
+//  member
+//    type names the same type as `std::common_type<T0, T0>::type` if it exists;
+//    otherwise there is no member type.
+//  * If `sizeof...(T) is two:
+//      * If applying ...
+//
+// The types in the parameter pack T shall each be a complete type, void, or an
+// array of unknown bound. Otherwise, the behavior is undefined.
+// If an instantiation of a template above depends, directly or indirectly, on
+// an incomplete type, and that instantiation could yield a different result
+// if that type were hypothetically completed, the behavior is undefined.
+
+template <typename T> inline T minimum_my(const T &t) { return t; }
+
+template <typename T, typename... P>
+typename std::common_type<T, P...>::type minimum_my(const T &t, const P &...p) {
+  using res_type = typename std::common_type<T, P...>::type;
+  return std::min(res_type(t), res_type(minimum_my(p...)));
+}
+
+template <typename T, typename... P>
+inline auto minimum_my_2(const T &t, const P &...p) {
+  using res_type = std::common_type_t<T, P...>;
+  return std::min(res_type(t), res_type(minimum_my_2(p...)));
+}
+
+} // namespace variadic_template_type_deduction
