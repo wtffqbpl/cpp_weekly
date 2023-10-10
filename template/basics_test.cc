1d5f75525681a5075155d35504611841fa3804ac
Yuanjun Ren <renyj@buaa.edu.cn>
2023-10-10 13:22:46 +0800

    add testcase for variadic template

--- template/basics_test.cc
+++ template/basics_test.cc
@@ -1,4 +1,5 @@
 #include "internal_check_conds.h"
+#include <deque>
 #include <gtest/gtest.h>
 #include <list>
 
@@ -214,6 +215,185 @@ void test_template_class_friend() {
   func2(mymen3);
 }
 
+// Variadic Templates
+template <typename... ClassPList> class MyClassT5 : public ClassPList... {
+public:
+  MyClassT5() : ClassPList()... {
+    std::cout
+        << "MyClassT5::MyClassT5." /* << " this = " << this */ << std::endl;
+  }
+};
+
+class PA1 {
+public:
+  PA1() { std::cout << "PA1::PA1. " /* << "this = " << this */ << std::endl; }
+
+private:
+  char m_s1[100];
+};
+
+class PA2 {
+public:
+  PA2() { std::cout << "PA2::PA2." /* << " this = " << this */ << std::endl; }
+
+private:
+  char m_s1[200];
+};
+
+class PA3 {
+public:
+  PA3() { std::cout << "PA3::PA3." /* << " this = " << this */ << std::endl; }
+
+private:
+  char m_s1[300];
+};
+
+// Variadic Template Partial Specialization
+template <typename... Args> class MyClassT6 {
+public:
+  MyClassT6() {
+    std::cout
+        << "MyClassT6 generic version is called." /* << " this = " << this */
+        << ", sizeof...(Args) = " << sizeof...(Args) << std::endl;
+  }
+};
+
+template <typename First, typename... Others>
+class MyClassT6<First, Others...> {
+public:
+  MyClassT6() {
+    std::cout << "MyClassT6<First, Others...> partial specialization version "
+                 "is called."
+              /* << ", this = " << this */
+              << ", sizeof...(Others) = " << sizeof...(Others) << std::endl;
+  }
+};
+
+template <typename Arg> class MyClassT6<Arg> {
+public:
+  MyClassT6() {
+    std::cout << "MyClassT6<Arg> partial specialization version is called."
+              /*<< ", this = " << this */
+              << std::endl;
+  }
+};
+
+template <typename Arg1, typename Arg2> class MyClassT6<Arg1, Arg2> {
+public:
+  MyClassT6() {
+    std::cout
+        << "MyClassT6<Arg1, Arg2> partial specialization version is called. "
+        /* << "this = " << this */
+        << std::endl;
+  }
+};
+
+template <typename... Args> class MyClassT;
+
+template <typename First, typename... Others>
+class MyClassT<First, Others...> : private MyClassT<Others...> {
+public:
+  MyClassT() : m_i(0) {
+    std::cout
+        << "MyClassT::MyclassT() partial specialization constructor is called."
+        << ", sizeof...(Others) = " << sizeof...(Others) << std::endl;
+  }
+
+  MyClassT(First parf, Others... paro)
+      : m_i(parf), MyClassT<Others...>(paro...) {
+    std::cout << "-------------------- BEGIN ------------------" << std::endl;
+    std::cout << "MyClassT::MyClassT(parf, ...paro) constructor is called."
+              << std::endl;
+    std::cout << "m_i = " << m_i << std::endl;
+    std::cout << "--------------------- END -------------------" << std::endl;
+  }
+
+private:
+  First m_i;
+};
+
+template <typename... Args> class MyClassT {
+public:
+  MyClassT() {
+    std::cout << "MyClassT::MyClassT() generic version constructor is called."
+              << std::endl;
+  }
+};
+
+template <int... FTArgs> class MyClassT2 {
+public:
+  MyClassT2() {
+    std::cout << "MyClassT2::MyClassT2() generic verion constructor is called."
+              << std::endl;
+  }
+};
+
+template <int First, int... Others>
+class MyClassT2<First, Others...> : private MyClassT2<Others...> {
+public:
+  MyClassT2() {
+    std::cout << "MyClassT2::MyClassT2() partial specialization version "
+                 "constructor is called."
+              << " sizeof...(Others) = " << sizeof...(Others)
+              << ", First = " << First << std::endl;
+  }
+};
+
+template <typename T, template <typename> typename... Container>
+class ParentMM {
+public:
+  ParentMM() {
+    std::cout << "ParentMM::ParentMM() generic version constructor is called."
+              << std::endl;
+  }
+};
+
+template <typename T, template <typename> typename FirstContainer,
+          template <typename> typename... OtherContainers>
+class ParentMM<T, FirstContainer, OtherContainers...>
+    : private ParentMM<T, OtherContainers...> {
+public:
+  ParentMM() {
+    std::cout << "parentMM::ParentMM() partial specialization version "
+                 "constructor is called."
+              << ", sizeof...(OtherContainers) = " << sizeof...(OtherContainers)
+              << std::endl;
+    m_container.push_back(12);
+  }
+
+private:
+  FirstContainer<T> m_container;
+};
+
+template <typename T, template <typename> typename... Container>
+class MyClassT3 : private ParentMM<T, Container...> {
+public:
+  MyClassT3() {
+    std::cout << "MyClassT3::MyClassT3() constructor is called."
+              << " The Type(T) is " << typeid(T).name()
+              << ", sizeof...(Container) = " << sizeof...(Container)
+              << std::endl;
+  }
+};
+
+void test_base_inherit_pack_expansion() {
+  MyClassT5<PA1, PA2, PA3> obj;
+  std::cout << "sizeof(obj) = " << sizeof(obj) << std::endl; // 600
+
+  MyClassT6<int> myc1;
+  MyClassT6<int, float> myc2;
+  MyClassT6<int, double> myc3;
+  MyClassT6<int, double, char> myc4;
+  MyClassT6<> myc5;
+
+  MyClassT<int, float, double> myc;
+  MyClassT<int, float, double> myc_2(12, 13.5, 23);
+
+  MyClassT2<12, 18, 23> myc2_1;
+
+  // MyClassT3<int, std::vector, std::list, std::deque> myc3_1;
+}
+
 } // namespace class_variable_alias_templates
 
 TEST(class_template, test1) {
@@ -302,6 +482,60 @@ TEST(friend_template, test1) {
       << "Men::funcmen is called\n"
       << "Men::funcmen is called\n"
       << "Men::funcmen is called\n";
+
+  auto act_output = testing::internal::GetCapturedStdout();
+
+#ifndef NDEBUG
+  debug_msg(oss, act_output);
+#endif
+
+  EXPECT_TRUE(oss.str() == act_output);
+}
+
+TEST(variadic_template, test1) {
+  std::stringstream oss;
+  testing::internal::CaptureStdout();
+
+  class_variable_alias_templates::test_base_inherit_pack_expansion();
+
+  oss << "PA1::PA1. \n"
+         "PA2::PA2.\n"
+         "PA3::PA3.\n"
+         "MyClassT5::MyClassT5.\n"
+         "sizeof(obj) = 600\n"
+         "MyClassT6<Arg> partial specialization version is called.\n"
+         "MyClassT6<Arg1, Arg2> partial specialization version is called. \n"
+         "MyClassT6<Arg1, Arg2> partial specialization version is called. \n"
+         "MyClassT6<First, Others...> partial specialization version is "
+         "called., sizeof...(Others) = 2\n"
+         "MyClassT6 generic version is called., sizeof...(Args) = 0\n"
+         "MyClassT::MyClassT() generic version constructor is called.\n"
+         "MyClassT::MyclassT() partial specialization constructor is called., "
+         "sizeof...(Others) = 0\n"
+         "MyClassT::MyclassT() partial specialization constructor is called., "
+         "sizeof...(Others) = 1\n"
+         "MyClassT::MyclassT() partial specialization constructor is called., "
+         "sizeof...(Others) = 2\n"
+         "MyClassT::MyClassT() generic version constructor is called.\n"
+         "-------------------- BEGIN ------------------\n"
+         "MyClassT::MyClassT(parf, ...paro) constructor is called.\n"
+         "m_i = 23\n"
+         "--------------------- END -------------------\n"
+         "-------------------- BEGIN ------------------\n"
+         "MyClassT::MyClassT(parf, ...paro) constructor is called.\n"
+         "m_i = 13.5\n"
+         "--------------------- END -------------------\n"
+         "-------------------- BEGIN ------------------\n"
+         "MyClassT::MyClassT(parf, ...paro) constructor is called.\n"
+         "m_i = 12\n"
+         "--------------------- END -------------------\n"
+         "MyClassT2::MyClassT2() generic verion constructor is called.\n"
+         "MyClassT2::MyClassT2() partial specialization version constructor is "
+         "called. sizeof...(Others) = 0, First = 23\n"
+         "MyClassT2::MyClassT2() partial specialization version constructor is "
+         "called. sizeof...(Others) = 1, First = 18\n"
+         "MyClassT2::MyClassT2() partial specialization version constructor is "
+         "called. sizeof...(Others) = 2, First = 12\n";
 
   auto act_output = testing::internal::GetCapturedStdout();
 
