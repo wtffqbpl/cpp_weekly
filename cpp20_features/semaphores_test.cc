ef4a21faaa313ef30b2e2197f0d3dbec77ac0851
Yuanjun Ren <renyj@buaa.edu.cn>
2023-12-26 22:17:56 +0800

    [CPP20]Add std::semaphore testcases

---
+++ cpp20_features/semaphores_test.cc
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include <queue>
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <semaphore>
+#include <stop_token>
+
+namespace {
+
+void semaphores_basic_test() {
+  std::queue<char> values; // queue of values
+  std::mutex values_mtx; // mutex to protect access to the queue
+
+  // initialize a queue with multiple sequences from 'a' to 'z':
+  for (int i = 0; i < 1000; ++i) {
+    values.push(static_cast<char>('a' + (i % ('z' - 'a'))));
+  }
+
+  // create a pool of num_threads threads:
+  // - limit their availability with a semaphore (initially none available):
+  constexpr int num_threads = 10;
+  std::counting_semaphore<num_threads> enabled{0};
+
+  // create and start all threads of the pool:
+  std::vector<std::thread> pool;
+  for (int idx = 0; idx < num_threads; ++idx) {
+    // std::thread t{[idx, &enabled, &values, &values_mtx] (std::stop_token st) { }};
+    // pool.push_back(std::move(t));
+  }
+}
+
+}
