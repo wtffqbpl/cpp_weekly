d22bca43116a9ce68e42db6c14472706d12309af
Yuanjun Ren <renyj@buaa.edu.cn>
2023-12-29 13:37:26 +0800

    [CPP20]Add std::format alignment and precison examples

--- cpp20_features/format_test.cc
+++ cpp20_features/format_test.cc
@@ -6,11 +6,55 @@
 
 namespace {
 
+void print2(const auto &arg1, const auto &arg2) {
+  std::cout << std::format("args: {} and {}\n", arg1, arg2);
+}
+
 void basic_example() {
   std::string str{"hello"};
 
+  // std::format
+  // It allows them to combine a formatting string to be filled with values of
+  // passed arguments according to the formatting specified inside a pair of
+  // braces.
   std::cout << std::format("String '{}' has {} chars\n", str, str.size());
+
+  // An optional integral value right after the opening braces specifies the
+  // index of the argument so that you can process the arguments in different
+  // order or use them multiple times.
   std::cout << std::format("String '{1}' has {0} chars\n", str, str.size());
+
+  print2(str, str.size());
+}
+
+void format_alignment_example() {
+
+  // Inside the placeholder for formatting after a colon, you can specify
+  // details of the formatting of the argument.
+  // Note that different types have different default alignments.
+  std::cout << std::format("{:7}\n", 42);   // yields "     42"
+  std::cout << std::format("{:7}\n", 42.0); // yields "     42"
+  std::cout << std::format("{:7}\n", 'x');  // yields "x      "
+  std::cout << std::format("{:7}\n", true); // yields "true   "
+
+  // You can also specify the alignment explicitly:
+  //  - < for left,
+  //  - ^ for center
+  //  - > for right
+  // And it allows to specify a fill character:
+  std::cout << std::format("{:*<7}\n", 42); // yields "42*****"
+  std::cout << std::format("{:*>7}\n", 42); // yields "*****42"
+  std::cout << std::format("{:*^7}\n", 42); // yields "**42***"
+
+  // Specify precision (for limit strings to a certain size), fill characters,
+  // or a positive sign:
+  std::cout << std::format("{:7.2f} Euro\n", 42.0); // yields "  42.00 Euro"
+  std::cout << std::format("{:7.4}\n", "corner");   // yields "corn   "
+
+  // By using the position of the argument, we can print a value in multiple
+  // forms.
+  std::cout << std::format("'{0}' has value 0x{0:02x} {0:+4d} {0:03o}\n", '?');
+  std::cout << std::format("'{0}' has value 0x{0:02x} {0:+4d} {0:03o}\n", 'y');
 }
 
 } // namespace
@@ -24,7 +68,23 @@ TEST(format_test, basic_test) {
   auto act_output = testing::internal::GetCapturedStdout();
 
   oss << "String 'hello' has 5 chars\n"
-         "String '5' has hello chars\n";
+         "String '5' has hello chars\n"
+         "args: hello and 5\n";
+
+#ifndef NDEBUG
+  debug_msg(oss, act_output);
+#endif
+
+  EXPECT_EQ(oss.str(), act_output);
+}
+
+TEST(format_test, formatting_alignment_test) {
+  std::stringstream oss;
+  testing::internal::CaptureStdout();
+
+  format_alignment_example();
+
+  auto act_output = testing::internal::GetCapturedStdout();
 
 #ifndef NDEBUG
   debug_msg(oss, act_output);
