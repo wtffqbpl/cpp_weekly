4db16199078c73f1bb56e74f8c847fbccb392f7c
Yuanjun Ren <renyj@buaa.edu.cn>
2024-01-02 12:48:48 +0800

    [CPP20]Add placeholder type examples

--- cpp20_features/placeholder_types.cc
+++ cpp20_features/placeholder_types.cc
@@ -2,6 +2,7 @@
 
 #ifdef __APPLE__
 
+#include <concepts>
 #include <gtest/gtest.h>
 #include <iostream>
 
@@ -79,6 +80,53 @@ public:
 
   void print() const {}
 };
+
+// Not that `auto` allows the caller to pass parameters of any type as long as
+// the type provides a member function `print()`. If you do not want that, you
+// can use the standard `concept std::same_as` to constrain the use of this
+// member function for parameters of type `C2` only. For the concept,
+// an incomplete type works fine.
+
+/// \code
+/// class C2;
+/// class C1WithConcept {
+/// public:
+///   void foo(const std::same_as<C2> auto &c2) const {
+///     c2.print(); // OK
+///   }
+///   void print() const;
+/// };
+
+// For a generic lambda, we can use the following step to specify types
+// explicitly.
+/// \code
+/// auto print_func = [] (const auto &arg) {};
+/// print_func.operator()<std::string>("hello")
+/// \endcode
+
+// Using `auto` to declare a function parameter follows the same rules as using
+// it to declare a parameter of a lambda:
+// For each parameter declared with `auto`, the function has an implicit
+// template parameter.
+//  * The parameters can be a parameter pack:
+//      void foo(auto... args);
+//    This is equivalent to the following (without introducing Types):
+//      template <typename... Types>
+//      void foo(Types... args);
+//  * Using decltype(auto) is not allowed.
+// Abbreviated function templates can still be called with (partially)
+// explicitly specified template parameters. The template parameters have the
+// order of the call parameters. For example:
+/// \code
+/// void foo(auto x, auto y)
+/// {
+///     ...
+/// }
+///
+/// foo("hello", 42);                   // x has type const char*, y has type
+/// int foo<std::string>("hello", 42);      // x has type std::string, y has
+/// type int foo<std::string, long>("hello", 42); // x has type std::string, y
+/// has type long \endcode
 
 } // namespace
 
