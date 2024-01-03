35e14617f3131311930bce3b07fb529c45043b35
Yuanjun Ren <renyj@buaa.edu.cn>
2024-01-03 09:36:29 +0800

    [CPP20]Add std::latch examples

--- cpp20_features/latches.cc
+++ cpp20_features/latches.cc
@@ -49,10 +49,97 @@ void latch_basic_test() {
   t1.join();
   t2.join();
 }
+
+// A boss-worker workflow using two std::latch
+
+std::latch work_done{6};
+std::latch go_home{1};
+
+std::mutex cout_mutex;
+
+void synchronized_out(const std::string &s) {
+  std::lock_guard<std::mutex> lo(cout_mutex);
+  std::cout << s;
+}
+
+class Worker {
+public:
+  explicit Worker(std::string n) : name(std::move(n)) {}
+
+  void operator()() {
+    // Notify the boss when work is done
+    synchronized_out(name + ": " + "Work done!\n");
+    work_done.count_down();
+
+    // Waiting before going home
+    go_home.wait();
+    synchronized_out(name + ": " + "Good bye!\n");
+  }
+
+private:
+  std::string name;
+};
+
+void boss_worker_workflow_test() {
+  std::cout << "BOSS: START WORKING! " << '\n';
+
+  std::vector<std::string> worker_name_list{
+      " Herb",      "  Scott",     "   Bjarne",
+      "    Andrei", "     Andrew", "      David",
+  };
+
+  std::vector<std::thread> worker_list;
+  worker_list.reserve(worker_name_list.size());
+
+  for (const auto &name : worker_name_list)
+    worker_list.emplace_back(Worker{name});
+
+  work_done.wait();
+
+  go_home.count_down();
+
+  for (auto &t : worker_list)
+    t.join();
+}
+
+std::latch work_done_self_managed{6};
+
+class WorkerSelfManaged {
+public:
+  explicit WorkerSelfManaged(std::string n) : name(std::move(n)) {}
+
+  void operator()() {
+    synchronized_out(name + ": " + "Work done!\n");
+    work_done_self_managed.arrive_and_wait();
+    synchronized_out(name + ": " + "See you tomorrow!\n");
+  }
+
+private:
+  std::string name;
+};
+
+void worker_self_managed_test() {
+  std::vector<std::string> worker_name_list{
+      " Herb",      "  Scott",     "   Bjarne",
+      "    Andrei", "     Andrew", "      David",
+  };
+
+  std::vector<std::thread> worker_list;
+  worker_list.reserve(worker_name_list.size());
+
+  for (const auto &name : worker_name_list)
+    worker_list.emplace_back(WorkerSelfManaged{name});
+
+  for (auto &t : worker_list)
+    t.join();
+}
 }
 
 TEST(latch_test, basic_test) { latch_basic_test(); }
 
+TEST(latch_test, test2) { boss_worker_workflow_test(); }
+
+TEST(latch_test, test3) { worker_self_managed_test(); }
 #endif // __APPLE__
 
 #endif // __cplusplus >= 202002L
