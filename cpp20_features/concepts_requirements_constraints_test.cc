f2d2568539b98a5bd94eb1ccd69ad1aa50f0ae10
Yuanjun Ren <renyj@buaa.edu.cn>
2024-01-05 13:13:35 +0800

    [CPP20]Add concept subsumptions testcases

--- cpp20_features/concepts_requirements_constraints_test.cc
+++ cpp20_features/concepts_requirements_constraints_test.cc
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
+
 #include <atomic>
+#include <bit>
 #include <concepts>
 #include <ranges>
 #include <set>
@@ -368,16 +370,15 @@ void add(Coll &coll, const T &val) {
 }
 
 template <typename Coll>
-concept SupportsPushBackV2 = requires(Coll coll, Coll::value_type val) {
-    coll.push_back(val);
-};
+concept SupportsPushBackV2 =
+    requires(Coll coll, Coll::value_type val) { coll.push_back(val); };
 // Note that we do not have to use `typename` here to use Coll::value_type.
 // Since C++20, `typename` is no longer required when it is clear by the context
 // that a qualified member must be a type.
 
 template <typename Coll>
 concept SupportsPushBackV3 = requires(Coll coll) {
-    coll.push_back(std::declval<typename Coll::value_type&>());
+  coll.push_back(std::declval<typename Coll::value_type &>());
 };
 // Note that the & is important here. Without &, we would only require that we
 // can insert an rvalue using move semantics. With &, we create an lvalue so
@@ -390,7 +391,8 @@ concept SupportsPushBackV4 = requires(Coll coll) {
 // In general, using std::ranges::range_value_t<> makes code more generic when
 // the element type of a collection is needed.
 
-// We can also use the concept SupportsPushBack directly in an `if constexpr` condition:
+// We can also use the concept SupportsPushBack directly in an `if constexpr`
+// condition:
 
 void foo(auto &coll, const auto &val) {
   if constexpr (SupportsPushBackV3<decltype(coll)>) {
@@ -498,26 +500,23 @@ namespace concept_test2 {
 
 // concept for container with push_back():
 template <typename Coll>
-concept SupportsPushBack = requires(Coll coll, Coll::value_type val) {
-  coll.push_back(val);
-};
+concept SupportsPushBack =
+    requires(Coll coll, Coll::value_type val) { coll.push_back(val); };
 
 // concept to disable narrowing conversion:
 template <typename From, typename To>
 concept ConvertsWithoutNarrowing =
-  std::convertible_to<From, To> &&
-      requires (From &&x) {
-        {
-          std::type_identity_t<To[]>{std::forward<From>(x)}
-        } -> std::same_as<To[1]>;
-};
+    std::convertible_to<From, To> && requires(From &&x) {
+      {
+        std::type_identity_t<To[]>{std::forward<From>(x)}
+      } -> std::same_as<To[1]>;
+    };
 
 // add() for single value:
 template <typename Coll, typename T>
-requires ConvertsWithoutNarrowing<T, typename Coll::value_type>
+  requires ConvertsWithoutNarrowing<T, typename Coll::value_type>
 void add(Coll &coll, const T &val) {
-  if constexpr (SupportsPushBack<Coll>)
-  {
+  if constexpr (SupportsPushBack<Coll>) {
     coll.push_back(val);
   } else {
     coll.insert(val);
@@ -526,8 +525,8 @@ void add(Coll &coll, const T &val) {
 
 // add() for multiple values:
 template <typename Coll, std::ranges::input_range T>
-requires ConvertsWithoutNarrowing<std::ranges::range_value_t<T>,
-                                  typename Coll::value_type>
+  requires ConvertsWithoutNarrowing<std::ranges::range_value_t<T>,
+                                    typename Coll::value_type>
 void add(Coll &coll, const T &val) {
   if constexpr (SupportsPushBack<Coll>) {
     coll.insert(coll.end(), std::ranges::begin(val), std::ranges::end(val));
@@ -563,7 +562,6 @@ void test() {
   int vals[] = {0, 8, 18};
   add(i_vec, vals);
   // add(d_vec, vals); // ERROR: narrowing
-
 }
 
 } // namespace concept_test2
@@ -618,10 +616,358 @@ namespace concept_test3 {
 // (especially as an if constexpr condition).
 
 template <typename T>
-requires (sizeof(T) > 4)                          // ad-hoc Boolean expression
+  requires(sizeof(T) > 4)                         // ad-hoc Boolean expression
           && requires { typename T::value_type; } // requires expression
           && std::input_iterator<T>               // concept
-void foo(T x)
+void foo(T x) {}
+
+template <typename Coll>
+  requires(sizeof(typename Coll::value_type) >= 4) && requires {
+    typename Coll::value_type::first_type;  // elements/values have first_type
+    typename Coll::value_type::second_type; // elements/values have second_type
+  }
+void foo2(Coll coll) {}
+
+template <typename T>
+  requires requires(T x, T y) {
+    x + y; // supports +
+    x - y; // supports -
+  }
+void foo3(T val) {}
+
+template <typename Coll>
+  requires requires(Coll::value_type v) {
+    std::cout << v; // supports output operator
+  }
+void foo4(Coll coll) {}
+
+// Simple requirements are just expressions that have to be well-formed. This
+// means that the calls have to compile. The calls are not performed, meaning
+// that it does not matter what the operations yield.
+// clang-format off
+template <typename T1, typename T2>
+requires requires(T1 val, T2 p) {
+    *p;    // operator* has to be supported for T2
+    p[0];  // operator[] has to be supported for int as index
+    p->value();  // calling a member function value() without arguments has to be possible
+    *p > val;    // support the comparison of the result of operator* with T1
+    p == nullptr; // support the comparison of a T2 with a nullptr
+  }
+void foo5(T1 x, T2 y) {}
+// clang-format on
+
+// To require either one of the two sub-expressions, you have to use:
+template <typename T1, typename T2>
+  requires requires(T1 val, T2 p) {
+    *p > val; // support the comparison of the result of operator* with T1
+  } || requires(T2 p) {
+    p == nullptr; // support the comparison of a T2 with nullptr
+  }
+auto foo2(T1 val, T2 p) {}
+
+// Type requirements
+// Type requirements are expressions that have to be well-formed when using a
+// name of a type. This means that the specified name has to be defined as a
+// valid type.
+template <typename T1, typename T2>
+  requires requires {
+    typename T1::value_type; // type member value_type required for T1
+    typename std::ranges::iterator_t<T1>; // iterator type required for T1
+    typename std::common_type_t<T1, T2>; // T1 and T2 have to have a common type
+  }
+void foo8(T1 coll, T2 val) {}
+
+// Compound Requirements
+// Compound requirements allow us to combine the abilities of simple and type
+// requirements. In this case, you can specify an expression (inside a block
+// of braces) and then add one or both of the following:
+//  * noexcept to require that the expression guarantees not to throw
+//  * type-constraint to apply a concept on what the expression evaluates to
+// Example:
+template <typename T>
+  requires requires(T x) {
+    { &x } -> std::input_or_output_iterator;
+    { x == x };
+    { x == x } -> std::convertible_to<bool>;
+    { x == x } noexcept;
+    { x == x } noexcept -> std::convertible_to<bool>;
+  }
+void foo10(T val) {}
+
+// We cannot use such a nested expression with a type trait in a result of a
+// requires expression:
+#if 0
+template <typename T>
+concept Check = requires(T p) {
+  { *p } -> std::integral<std::remove_reference_t<>>;   // ERROR
+  { *p } -> std::integral<std::remove_reference_t>;     // ERROR
+};
+#endif
+
+// You can either have to define a corresponding concept first:
+template <typename T>
+concept UnrefIntegral = std::integral<std::remove_reference_t<T>>;
+
+template <typename T>
+concept Check = requires(T p) {
+  { *p } -> UnrefIntegral; // OK
+};
+
+template <typename T>
+  requires requires(T coll) {
+    { *coll.begin() } -> std::convertible_to<typename T::value_type>;
+  }
+void foo11(T coll) {}
+// This is equivalent to
+template <typename T>
+  requires std::convertible_to<decltype(*std::declval<T>().begin()),
+                               typename T::value_type>
+void foo11(T coll) {}
+
+// Nested Requirements
+// Nested requirements can be used to specify additional constraints inside a
+// requires expression. They start with requires followed by a compile-time
+// Boolean expression, which might itself again be or use a requires expression.
+// The benefit of nested requirements is that we can ensure that a compile-time
+// expression (that uses parameters or sub-expressions of the requires
+// expression) yields a certain result instead of ensuring only that the
+// expression is valid.
+
+// Ensure that both the operator* and the operator[] yield the same type for a
+// given type. By using nested requirements, we can specify this as follows:
+template <typename T>
+concept DerefAndIndexMatch =
+    requires(T p) { requires std::same_as<decltype(*p), decltype(p[0])>; };
+
+// We have an syntax here for "assume we have an object of type T."
+template <typename T>
+concept DerefAndIndexMatch2 =
+    std::same_as<decltype(*std::declval<T>()), decltype(std::declval<T>()[0])>;
+
+template <typename T>
+concept Check2 = requires(T p) {
+  requires std::integral<std::remove_cvref_t<decltype(*p)>>;
+};
+// This is equivalent to:
+template <typename T>
+concept Check2_alias = requires {
+  !std::is_const_v<T>;          // OOPS: checks whether we can call is_const_v<>
+  requires !std::is_const_v<T>; // OK: checks whether T is not const
+};
+//  * The first expression requires only that checking for constness and
+//  negating
+//    the result is valid. This requirement is always met (even if T is const
+//    int) because doing this check is always valid. This requirement is
+//    worthless.
+//  * The second expression with requires has to be fulfilled. The requirement
+//    is met if T is int but not if T is const int.
+
+} // namespace concept_test3
+
+namespace concepts_test4 {
+// By defining a concept, you can introduce a name for one or more constraints.
+// Templates (function, class, variable, and alias templates) can use concepts
+// constrain their ability (via a requires clause or as a direct type constraint
+// for a template parameter). However, concepts are also Boolean compile-time
+// expressions (type predicates) that you can use wherever you have to check
+// something for a type (such as in an if-constexpr condition).
+// Defining Concepts:
+/// \code
+/// template <...>
+/// concept name = ... ;
+/// \endcode
+// The equal sign is required (you cannot declare a concept without defining
+// it and you cannot use braces here). After the equal sign, you can specify
+// any compile-time expression that converts to true or false.
+// Concepts are much like constexpr variable templates of type bool, but the
+// type is not explicitly specified:
+/// \code
+/// template <typename T>
+/// concept MyConcept = ... ;
+///
+/// std::is_same<MyConcept<...>, bool>  // yields true
+/// \endcode
+// This means that both at compile time and at runtime you can always use a
+// concept where the value of a Boolean expression is needed. However, you
+// cannot take the address because there is no object behind it (it is a
+// prvalue) The template parameters may not have constriants (you cannot use a
+// concept to define a concept). You cannot define concepts inside a function
+// (as is the case for all templates).
+
+// Special Abilities of Concepts:
+//  * Concepts do not represent code. They have no type, storage, lifetime, or
+//    any other properties associated with objects.
+//  * Concepts do not have to be declared as inline. They implicitly are inline.
+//  * Concepts can be used as type constraints.
+//  * Concepts are the only way to give constraints a name, which means that you
+//    need them to decide whether a constraint is a special case of another
+//    constraint.
+//  * Concepts subsume. To let the compiler decide whether a constraint implies
+//    another constraint (and is therefore special), the constraints have to be
+//    formulated as concepts.
+
+// Concepts for Non-Type Template Paramters
+// Concepts can also be applied to non-type template parameters (NTTP).
+template <auto Val>
+concept LessThan10 = Val < 10;
+
+template <int Val>
+  requires LessThan10<Val>
+class MyType {};
+
+// As a more useful example, we can use a concept to constrain the value of a
+// non-type template parameter to be a power of two:
+template <auto Val>
+concept PowerOf2 = std::has_single_bit(static_cast<unsigned>(Val));
+
+template <typename T, auto Val>
+  requires PowerOf2<Val>
+class Memory {};
+
+// Memory<int, 8> m1;    // OK
+// Memory<int, 9> m2;    // ERROR
+// Memory<int, 32> m3;   // OK
+// Memory<int, true> m4; // OK
+
+// Using Concepts as Type Constraints
+// Concepts can be used as type constraints. There are different places where
+// type constraints can be used:
+//  * In the declaration of a template type parameter.
+//  * In the declaration of a call parameter declared with auto
+//  * As a requirement in a compound requirement
+
+template <std::integral T> // type constraint for a template parameter
+class MyClass {};
+
+auto my_func(
+    const std::integral auto &val) // type constraints for an auto parameter
 {}
 
+template <typename T>
+concept MyConcept = requires(T x) {
+  { x + x } -> std::integral; // Type constraint for return type
+};
+
+// Type constraints can be used in all places where auto can be used. The major
+// application of this feature is to use the type constraints for the function
+// parameters declared with auto. For example:
+void foo(const std::integral auto &val) {}
+
+// To constrain declarations:
+// std::floating_point auto val1 = f();
+// for (const std::integral auto &elem : coll { // valid if elements are
+// integral values
+// }
+
+// To constrain return types:
+// std::copyable auto foo(auto) {} // valid if foo() returns copyable value
+
+// To constrain non-type template paramters:
+template <typename T, std::integral auto Max> class SizedColl {};
+
+// This also works with concepts that take multiple parameters:
+template <typename T, std::convertible_to<T> auto DefaultValue>
+class MyType2 {};
+
+} // namespace concepts_test4
+
+namespace concept_subsuming_test {
+// Two concepts can have a subsuming relationship, that is, one concept can be
+// specified such that it restricts one or more other concepts. The benefit is
+// that overload resolution then prefers the more constrained generic code over
+// the less constrained generic code when both constraints are satisfied.
+template <typename T>
+concept GeoObject = requires(T obj) {
+  { obj.width() } -> std::integral;
+  { obj.height() } -> std::integral;
+  obj.draw();
+};
+
+class Color {};
+
+template <typename T>
+concept ColoredGeoObject = GeoObject<T> && requires(T obj) {
+  obj.setColor(Color{});
+  { obj.getColor() } -> std::convertible_to<Color>;
+};
+
+template <GeoObject T>
+void process(
+    T) // called for objects that do not provide setColor() and getColor()
+{}
+
+template <ColoredGeoObject T>
+void process(T) // called for objects that provide setColor() and getColor()
+{}
+
+// Constraint subsumption works only when concepts are used. There is no
+// automatic subsumption when one concept/constraint is more special than
+// another.
+
+// The effort to check for subsumptions is evaluated only for concepts.
+// Overloading with different constraints is ambiguous if no concepts are used.
+// For example:
+#if 0
+template <typename T>
+  requires std::is_convertible_v<T, int>
+void print2(T) {}
+
+template <typename T>
+  requires(std::is_convertible_v<T, int> && sizeof(int) >= 4)
+void print2(T) {}
+#else
+// When using concepts instead, this code works:
+template <typename T>
+  requires std::convertible_to<T, int>
+void print2(T) {}
+
+template <typename T>
+  requires(std::convertible_to<T, int> && sizeof(T) >= 4)
+void print2(T) {}
+#endif
+
+// Indirect Subsumptions
+// Constraints can even subsume indirectly. This means that overload resolution
+// can still prefer one overload or specialization over the other, even though
+// their constraints are not defined in terms of each other.
+template <typename T>
+concept RgSwap = std::ranges::input_range<T> && std::swappable<T>;
+
+template <typename T>
+concept ContCopy = std::ranges::contiguous_range<T> && std::copyable<T>;
+
+template <RgSwap T> void foo1(T) { std::cout << "foo1(RgSwap)\n"; }
+
+template <ContCopy T> void foo1(T) { std::cout << "foo1(ContCopy)\n"; }
+
+void concepts_indirect_subsumption_test() {
+  foo1(std::vector<int>{}); // OK: both fit, ContCopy is more constrained
+}
+// The reason is that ContCopy subsumes RgSwap because:
+//  * The concept contiguous_range is defined in terms of the concept
+//    input_range. It implies random_access_range, which implies
+//    bidirectional_range, which implies forward_range, which implies
+//    input_range.
+//  * The concept copyable is defined in terms of the concept swappable. (It
+//    implies movable, which implies swappable.)
+
+} // namespace concept_subsuming_test
+
+TEST(concept_test, indirect_subsumption_test) {
+  using namespace concept_subsuming_test;
+
+  std::stringstream oss;
+  testing::internal::CaptureStdout();
+
+  concepts_indirect_subsumption_test();
+
+  oss << "foo1(ContCopy)\n";
+
+  auto act_output = testing::internal::GetCapturedStdout();
+
+#ifndef NDEBUG
+  debug_msg(oss, act_output);
+#endif
+
+  EXPECT_EQ(oss.str(), act_output);
 }
